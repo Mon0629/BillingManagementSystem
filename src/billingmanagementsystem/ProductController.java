85f881404a7f4884195e1612ecf4f2ab9a05e39a
Seiffer <88134704+Torehpies@users.noreply.github.com>
2023-12-09 23:34:29 +0800

    Updated Products Controllers

--- src/billingmanagementsystem/ProductController.java
+++ src/billingmanagementsystem/ProductController.java
@@ -42,198 +42,197 @@ import javafx.stage.StageStyle;
  */
 public class ProductController implements Initializable {
 
-    @FXML
-    private TableView<ProductData> product_table;
-    @FXML
-    private TableColumn<ProductData, Integer> productID;
-    @FXML
-    private TableColumn<ProductData, String> productName;
-    @FXML
-    private TableColumn<ProductData, Double> price;
-    @FXML
-    private TableColumn<ProductData, String> Description;
-    @FXML
-    private TableColumn<ProductData, Boolean> editcolumn;
-    @FXML
-    private TextField searchbox;
-    @FXML
-    private TableColumn<ProductData, Integer> Stocks;
-    @FXML
-    private TableColumn<ProductData, String> parentType;
-    @FXML
-    private TableColumn<ProductData, String> type;
-    @FXML
-    private TableColumn<ProductData, String> Brand;
-    @FXML
-    private TableColumn<ProductData, String> othersAttributes;
-    @FXML
-    private TableColumn<ProductData, Image> ImageCol;
+	@FXML
+	private TableView<ProductData> product_table;
+	@FXML
+	private TableColumn<ProductData, Integer> productID;
+	@FXML
+	private TableColumn<ProductData, String> productName;
+	@FXML
+	private TableColumn<ProductData, Double> price;
+	@FXML
+	private TableColumn<ProductData, String> Description;
+	@FXML
+	private TableColumn<ProductData, Boolean> editcolumn;
+	@FXML
+	private TextField searchbox;
+	@FXML
+	private TableColumn<ProductData, Integer> Stocks;
+	@FXML
+	private TableColumn<ProductData, String> parentType;
+	@FXML
+	private TableColumn<ProductData, String> type;
+	@FXML
+	private TableColumn<ProductData, String> Brand;
+	@FXML
+	private TableColumn<ProductData, String> othersAttributes;
+	@FXML
+	private TableColumn<ProductData, Image> ImageCol;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        ProductTable();
-    }    
-
-    
-     //loading prod into the table
-    
-    private ObservableList<ProductData> retrieveProductsFromDatabase() throws SQLException {
-        ObservableList<ProductData> productList = FXCollections.observableArrayList();
-        
-        try (Connection con = DatabaseManager.getConnection();
-             Statement st = con.createStatement();
-             ResultSet rs = st.executeQuery("SELECT * FROM Products join ProductCategory on Products.ProductID = ProductCategory.ProductID")) {
-
-            while (rs.next()) {
-                productList.add(new ProductData(
-                        rs.getInt("ProductID"),
-                        rs.getString("ProductName"),
-                        rs.getDouble("Price"),
-                        rs.getInt("Stocks"),
-                        rs.getString("Description"),
-                        (Blob) rs.getBlob("Image"),
-                        rs.getString("parentType"),
-                        rs.getString("type"),
-                        rs.getString("Brand"),
-                rs.getString("otherAttributes")
-                ));
-            }
-        }
-
-        return productList;
-    }
-    
-    
-    
-     private void ProductTable() {
-        try {
-        product_table.getItems().setAll(retrieveProductsFromDatabase());
-        productID.setCellValueFactory(new PropertyValueFactory<>("ProductID"));
-        productName.setCellValueFactory(new PropertyValueFactory<>("ProductName"));
-        price.setCellValueFactory(new PropertyValueFactory<>("Price"));
-        Stocks.setCellValueFactory(new PropertyValueFactory<>("Stocks"));
-        Description.setCellValueFactory(new PropertyValueFactory<>("Description"));
-        parentType.setCellValueFactory(new PropertyValueFactory("parentType"));
-        type.setCellValueFactory(new PropertyValueFactory("type"));
-        Brand.setCellValueFactory(new PropertyValueFactory("Brand"));
-        othersAttributes.setCellValueFactory(new PropertyValueFactory("otherAttributes"));
-        ImageCol.setCellValueFactory(new PropertyValueFactory("Image"));
-        ImageCol.setCellFactory(param -> new TableCell<ProductData, Image>() {
-   
-            private final ImageView imageView = new ImageView();
-
-             @Override
-             protected void updateItem(Image item, boolean empty) {
-              super.updateItem(item, empty);
-               if (empty || item == null) {
-            setGraphic(null);
-            } else {
-            imageView.setImage(item);
-            imageView.setFitWidth(50); // Set the desired width
-            imageView.setPreserveRatio(true);
-            setGraphic(imageView);
-          }
-         }
-        });
-        
-        editcolumn.setCellFactory(new ButtonCellFactory());
-        editcolumn.setCellFactory(new ButtonCellFactory()); 
-    } catch (SQLException ex) {
-        Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
-    }
-}
-
-     
-     
-     
-     
-     void refresh(){
-     ProductTable();
-     }
-     
-
-    @FXML
-    private void SearchProduct(KeyEvent event) {
-        String input = searchbox.getText().toLowerCase();
-
-    if (!input.isEmpty()) {
-            try {
-                ObservableList<ProductData> originalList = retrieveProductsFromDatabase();
-                ObservableList<ProductData> filteredList = FXCollections.observableArrayList();
-                
-                for (ProductData product : originalList) {
-                    if (matchesCriteria(product, input)) {
-                        filteredList.add(product);
-                    }
-                }
-                
-                product_table.getItems().setAll(filteredList);
-            } catch (SQLException ex) {
-                Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-    } else {
-        product_table.getItems().clear();
-        refresh();
-    }
-    }
-    
-    private boolean matchesCriteria(ProductData product, String input) {
-    String[] criteriaArray = input.split("\\s+");
-    boolean matchesAllCriteria = true;
-
-    for (String criterion : criteriaArray) {
-        matchesAllCriteria = matchesAllCriteria &&
-                (String.valueOf(product.getProductID()).contains(criterion)
-                || product.getProductName().toLowerCase().contains(criterion)
-                || product.getParentType().toLowerCase().contains(criterion)
-                || product.getType().toLowerCase().contains(criterion)
-                || product.getBrand().toLowerCase().contains(criterion)
-                || product.getOtherAttributes().toLowerCase().contains(criterion));
-    }
-
-    return matchesAllCriteria;
-}
+	/**
+	 * Initializes the controller class.
+	 */
+	 @Override
+	 public void initialize(URL url, ResourceBundle rb) {
+		 ProductTable();
+	 }    
 
 
-    @FXML
-    private void OpenProductDetails(MouseEvent event) {
-    try {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("ProductDetails.fxml"));
-        
-       
-                
-        Parent parent = loader.load();
-        
-         ProductDetailsController controller = loader.getController();
-         controller.setProductController(this);
-  
-        Scene scene = new Scene(parent);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.initStyle(StageStyle.UTILITY);
-        stage.show();
+	 //loading prod into the table
 
-    } catch (IOException ex) {
-        Logger.getLogger(CustomerController.class.getName()).log(Level.SEVERE, null, ex);
-    }
-}
+	 private ObservableList<ProductData> retrieveProductsFromDatabase() throws SQLException {
+		 ObservableList<ProductData> productList = FXCollections.observableArrayList();
+
+		 try (Connection con = DatabaseManager.getConnection();
+				 Statement st = con.createStatement();
+				 ResultSet rs = st.executeQuery("SELECT * FROM Products join ProductCategory on Products.ProductID = ProductCategory.ProductID")) {
+			 while (rs.next()) {
+				 productList.add(new ProductData(
+						 rs.getInt("ProductID"),
+						 rs.getString("ProductName"),
+						 rs.getDouble("Price"),
+						 rs.getInt("Stocks"),
+						 rs.getString("Description"),
+						 (Blob) rs.getBlob("Image"),
+						 rs.getString("parentType"),
+						 rs.getString("type"),
+						 rs.getString("Brand"),
+						 rs.getString("otherAttributes")
+						 ));
+			 }
+		 }
+
+		 return productList;
+	 }
 
 
-    @FXML
-    private void RefreshTable(MouseEvent event) {
-        refresh();
-       /* try {
+
+	 private void ProductTable() {
+		 try {
+			 product_table.getItems().setAll(retrieveProductsFromDatabase());
+			 productID.setCellValueFactory(new PropertyValueFactory<>("ProductID"));
+			 productName.setCellValueFactory(new PropertyValueFactory<>("ProductName"));
+			 price.setCellValueFactory(new PropertyValueFactory<>("Price"));
+			 Stocks.setCellValueFactory(new PropertyValueFactory<>("Stocks"));
+			 Description.setCellValueFactory(new PropertyValueFactory<>("Description"));
+			 parentType.setCellValueFactory(new PropertyValueFactory("parentType"));
+			 type.setCellValueFactory(new PropertyValueFactory("type"));
+			 Brand.setCellValueFactory(new PropertyValueFactory("Brand"));
+			 othersAttributes.setCellValueFactory(new PropertyValueFactory("otherAttributes"));
+			 ImageCol.setCellValueFactory(new PropertyValueFactory("Image"));
+			 ImageCol.setCellFactory(param -> new TableCell<ProductData, Image>() {
+
+				 private final ImageView imageView = new ImageView();
+
+				 @Override
+				 protected void updateItem(Image item, boolean empty) {
+					 super.updateItem(item, empty);
+					 if (empty || item == null) {
+						 setGraphic(null);
+					 } else {
+						 imageView.setImage(item);
+						 imageView.setFitWidth(50); // Set the desired width
+						 imageView.setPreserveRatio(true);
+						 setGraphic(imageView);
+					 }
+				 }
+			 });
+
+			 editcolumn.setCellFactory(new ButtonCellFactory());
+			 editcolumn.setCellFactory(new ButtonCellFactory()); 
+		 } catch (SQLException ex) {
+			 Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
+		 }
+	 }
+
+
+
+
+
+	 void refresh(){
+		 ProductTable();
+	 }
+
+
+	 @FXML
+	 private void SearchProduct(KeyEvent event) {
+		 String input = searchbox.getText().toLowerCase();
+
+		 if (!input.isEmpty()) {
+			 try {
+				 ObservableList<ProductData> originalList = retrieveProductsFromDatabase();
+				 ObservableList<ProductData> filteredList = FXCollections.observableArrayList();
+
+				 for (ProductData product : originalList) {
+					 if (matchesCriteria(product, input)) {
+						 filteredList.add(product);
+					 }
+				 }
+
+				 product_table.getItems().setAll(filteredList);
+			 } catch (SQLException ex) {
+				 Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
+			 }
+		 } else {
+			 product_table.getItems().clear();
+			 refresh();
+		 }
+	 }
+
+	 private boolean matchesCriteria(ProductData product, String input) {
+		 String[] criteriaArray = input.split("\\s+");
+		 boolean matchesAllCriteria = true;
+
+		 for (String criterion : criteriaArray) {
+			 matchesAllCriteria = matchesAllCriteria &&
+					 (String.valueOf(product.getProductID()).contains(criterion)
+							 || product.getProductName().toLowerCase().contains(criterion)
+							 || product.getParentType().toLowerCase().contains(criterion)
+							 || product.getType().toLowerCase().contains(criterion)
+							 || product.getBrand().toLowerCase().contains(criterion)
+							 || product.getOtherAttributes().toLowerCase().contains(criterion));
+		 }
+
+		 return matchesAllCriteria;
+	 }
+
+
+	 @FXML
+	 private void OpenProductDetails(MouseEvent event) {
+		 try {
+			 FXMLLoader loader = new FXMLLoader(getClass().getResource("ProductDetails.fxml"));
+
+
+
+			 Parent parent = loader.load();
+
+			 ProductDetailsController controller = loader.getController();
+			 controller.setProductController(this);
+
+			 Scene scene = new Scene(parent);
+			 Stage stage = new Stage();
+			 stage.setScene(scene);
+			 stage.initModality(Modality.APPLICATION_MODAL);
+			 stage.initStyle(StageStyle.UTILITY);
+			 stage.show();
+
+		 } catch (IOException ex) {
+			 Logger.getLogger(CustomerController.class.getName()).log(Level.SEVERE, null, ex);
+		 }
+	 }
+
+
+	 @FXML
+	 private void RefreshTable(MouseEvent event) {
+		 refresh();
+		 /* try {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Invoice.fxml"));
-        
-       
-                
+
+
+
         Parent parent = loader.load();
- 
-  
+
+
         Scene scene = new Scene(parent);
         Stage stage = new Stage();
         stage.setScene(scene);
@@ -246,6 +245,6 @@ public class ProductController implements Initializable {
     }
     } */
 
-    
-    }  
+
+	 }  
 }
--- src/billingmanagementsystem/ProductTableController.java
+++ src/billingmanagementsystem/ProductTableController.java
@@ -36,197 +36,197 @@ import javafx.stage.Stage;
  */
 public class ProductTableController implements Initializable {
 
-    @FXML
-    private TableView<ProductData> product_table;
-    @FXML
-    private TableColumn<ProductData, Integer> productID;
-    @FXML
-    private TableColumn<ProductData, String> productName;
-    @FXML
-    private TableColumn<ProductData, Double> price;
-    @FXML
-    private TableColumn<ProductData, String> description;
-    @FXML
-    private TextField searchbox;
-    @FXML
-    private TableColumn<ProductData, Integer> stocks;
-    @FXML
-    private TableColumn<ProductData, String> swcategory;
-    @FXML
-    private TableColumn<ProductData, String> subcategory;
-    @FXML
-    private TableColumn<ProductData, String> brandcol;
-    @FXML
-    private TableColumn<ProductData, String> otherscol;
-    @FXML
-    private TableColumn<ProductData, Image> imagecol;
+	@FXML
+	private TableView<ProductData> product_table;
+	@FXML
+	private TableColumn<ProductData, Integer> productID;
+	@FXML
+	private TableColumn<ProductData, String> productName;
+	@FXML
+	private TableColumn<ProductData, Double> price;
+	@FXML
+	private TableColumn<ProductData, String> description;
+	@FXML
+	private TextField searchbox;
+	@FXML
+	private TableColumn<ProductData, Integer> stocks;
+	@FXML
+	private TableColumn<ProductData, String> swcategory;
+	@FXML
+	private TableColumn<ProductData, String> subcategory;
+	@FXML
+	private TableColumn<ProductData, String> brandcol;
+	@FXML
+	private TableColumn<ProductData, String> otherscol;
+	@FXML
+	private TableColumn<ProductData, Image> imagecol;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-       ProductTable();
-    }    
+	/**
+	 * Initializes the controller class.
+	 */
+	 @Override
+	 public void initialize(URL url, ResourceBundle rb) {
+		 ProductTable();
+	 }    
 
-    
-    
-    private void ProductTable(){
-        try
-        {
-            Connection con = DatabaseManager.getConnection();
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM products JOIN productcategory on products.ProductID = productcategory.ProductID");
-            
-            while (rs.next()) {
-                product_table.getItems().add(new ProductData(
-                        rs.getInt("ProductID"),
-                        rs.getString("ProductName"),
-                        rs.getDouble("Price"),
-                        rs.getInt("Stocks"),
-                        rs.getString("Description"),
-                        (Blob) rs.getBlob("Image"),
-                        rs.getString("parentType"),
-                        rs.getString("type"),
-                        rs.getString("Brand"),
-                rs.getString("otherAttributes")
-                ));
-            }
-            
-            
-        } catch (SQLException ex)
-        {
-            Logger.getLogger(ProductTableController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        productID.setCellValueFactory(new PropertyValueFactory<>("ProductID"));
-        productName.setCellValueFactory(new PropertyValueFactory<>("ProductName"));
-        price.setCellValueFactory(new PropertyValueFactory<>("Price"));
-        description.setCellValueFactory(new PropertyValueFactory<>("Description"));
-        stocks.setCellValueFactory(new PropertyValueFactory<>("Stocks"));
-        swcategory.setCellValueFactory(new PropertyValueFactory<>("parentType"));
-        subcategory.setCellValueFactory(new PropertyValueFactory<>("Type"));
-        brandcol.setCellValueFactory(new PropertyValueFactory<>("Brand"));
-        otherscol.setCellValueFactory(new PropertyValueFactory<>("otherAttributes"));
-        imagecol.setCellValueFactory(new PropertyValueFactory<>("Image"));
-        imagecol.setCellFactory(param -> new TableCell<ProductData, Image>() {
-   
-            private final ImageView imageView = new ImageView();
 
-             @Override
-             protected void updateItem(Image item, boolean empty) {
-              super.updateItem(item, empty);
-               if (empty || item == null) {
-            setGraphic(null);
-            } else {
-            imageView.setImage(item);
-            imageView.setFitWidth(50); // Set the desired width
-            imageView.setPreserveRatio(true);
-            setGraphic(imageView);
-          }
-         }
-        });
-    
-    
-    }
-    
-    @FXML
-    private void tableclicked(MouseEvent event) {
-        
-        if (event.getClickCount() == 1) {
-        ProductData product = product_table.getSelectionModel().getSelectedItem();
-        if (product != null && billingsController != null) {
-            billingsController.setProductData(product);
-            ((Node) (event.getSource())).getScene().getWindow().hide();
-        }
-    }
-}
-    
-void refresh(){
-ProductTable();
-}
-    
-//para to sa billing, para makuha yung instance ng controller
-       private BillingsController billingsController;
 
-    public void setBillingsController(BillingsController controller) {
-    this.billingsController = controller;
-    }   
+	 private void ProductTable(){
+		 try
+		 {
+			 Connection con = DatabaseManager.getConnection();
+			 Statement st = con.createStatement();
+			 ResultSet rs = st.executeQuery("SELECT * FROM products JOIN productcategory on products.ProductID = productcategory.ProductID");
 
-    @FXML
-    private void searchproduct(KeyEvent event) {
-        String input = searchbox.getText().toLowerCase();
+			 while (rs.next()) {
+				 product_table.getItems().add(new ProductData(
+						 rs.getInt("ProductID"),
+						 rs.getString("ProductName"),
+						 rs.getDouble("Price"),
+						 rs.getInt("Stocks"),
+						 rs.getString("Description"),
+						 (Blob) rs.getBlob("Image"),
+						 rs.getString("parentType"),
+						 rs.getString("type"),
+						 rs.getString("Brand"),
+						 rs.getString("otherAttributes")
+						 ));
+			 }
 
-    if (!input.isEmpty()) {
-            try {
-                ObservableList<ProductData> originalList = retrieveProductsFromDatabase();
-                ObservableList<ProductData> filteredList = FXCollections.observableArrayList();
-                
-                for (ProductData product : originalList) {
-                    if (matchesCriteria(product, input)) {
-                        filteredList.add(product);
-                    }
-                }
-                
-                product_table.getItems().setAll(filteredList);
-            } catch (SQLException ex) {
-                Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-    } else {
-             product_table.getItems().clear();
-              refresh();  
-    
-    }
-    }
-    
-     private boolean matchesCriteria(ProductData product, String input) {
-    String[] criteriaArray = input.split("\\s+");
-    boolean matchesAllCriteria = true;
 
-    for (String criterion : criteriaArray) {
-        matchesAllCriteria = matchesAllCriteria &&
-                (String.valueOf(product.getProductID()).contains(criterion)
-                || product.getProductName().toLowerCase().contains(criterion)
-                || product.getParentType().toLowerCase().contains(criterion)
-                || product.getType().toLowerCase().contains(criterion)
-                || product.getBrand().toLowerCase().contains(criterion)
-                || product.getOtherAttributes().toLowerCase().contains(criterion));
-    }
+		 } catch (SQLException ex)
+		 {
+			 Logger.getLogger(ProductTableController.class.getName()).log(Level.SEVERE, null, ex);
+		 }
 
-    return matchesAllCriteria;
-}
+		 productID.setCellValueFactory(new PropertyValueFactory<>("ProductID"));
+		 productName.setCellValueFactory(new PropertyValueFactory<>("ProductName"));
+		 price.setCellValueFactory(new PropertyValueFactory<>("Price"));
+		 description.setCellValueFactory(new PropertyValueFactory<>("Description"));
+		 stocks.setCellValueFactory(new PropertyValueFactory<>("Stocks"));
+		 swcategory.setCellValueFactory(new PropertyValueFactory<>("parentType"));
+		 subcategory.setCellValueFactory(new PropertyValueFactory<>("Type"));
+		 brandcol.setCellValueFactory(new PropertyValueFactory<>("Brand"));
+		 otherscol.setCellValueFactory(new PropertyValueFactory<>("otherAttributes"));
+		 imagecol.setCellValueFactory(new PropertyValueFactory<>("Image"));
+		 imagecol.setCellFactory(param -> new TableCell<ProductData, Image>() {
 
-     private ObservableList<ProductData> retrieveProductsFromDatabase() throws SQLException {
-        ObservableList<ProductData> productList = FXCollections.observableArrayList();
-        
-        try (Connection con = DatabaseManager.getConnection();
-             Statement st = con.createStatement();
-             ResultSet rs = st.executeQuery("SELECT * FROM Products join ProductCategory on Products.ProductID = ProductCategory.ProductID")) {
+			 private final ImageView imageView = new ImageView();
 
-            while (rs.next()) {
-                productList.add(new ProductData(
-                        rs.getInt("ProductID"),
-                        rs.getString("ProductName"),
-                        rs.getDouble("Price"),
-                        rs.getInt("Stocks"),
-                        rs.getString("Description"),
-                        (Blob) rs.getBlob("Image"),
-                        rs.getString("parentType"),
-                        rs.getString("type"),
-                        rs.getString("Brand"),
-                rs.getString("otherAttributes")
-                ));
-            }
-        }
+			 @Override
+			 protected void updateItem(Image item, boolean empty) {
+				 super.updateItem(item, empty);
+				 if (empty || item == null) {
+					 setGraphic(null);
+				 } else {
+					 imageView.setImage(item);
+					 imageView.setFitWidth(50); // Set the desired width
+					 imageView.setPreserveRatio(true);
+					 setGraphic(imageView);
+				 }
+			 }
+		 });
 
-        return productList;
-    }
-    
-    @FXML
-    private void Close(MouseEvent event) {
-        Stage stage = (Stage) ((ImageView) event.getSource()).getScene().getWindow();
-        stage.close();
-    }
-    
+
+	 }
+
+	 @FXML
+	 private void tableclicked(MouseEvent event) {
+
+		 if (event.getClickCount() == 1) {
+			 ProductData product = product_table.getSelectionModel().getSelectedItem();
+			 if (product != null && billingsController != null) {
+				 billingsController.setProductData(product);
+				 ((Node) (event.getSource())).getScene().getWindow().hide();
+			 }
+		 }
+	 }
+
+	 void refresh(){
+		 ProductTable();
+	 }
+
+	 //para to sa billing, para makuha yung instance ng controller
+	 private BillingsController billingsController;
+
+	 public void setBillingsController(BillingsController controller) {
+		 this.billingsController = controller;
+	 }   
+
+	 @FXML
+	 private void searchproduct(KeyEvent event) {
+		 String input = searchbox.getText().toLowerCase();
+
+		 if (!input.isEmpty()) {
+			 try {
+				 ObservableList<ProductData> originalList = retrieveProductsFromDatabase();
+				 ObservableList<ProductData> filteredList = FXCollections.observableArrayList();
+
+				 for (ProductData product : originalList) {
+					 if (matchesCriteria(product, input)) {
+						 filteredList.add(product);
+					 }
+				 }
+
+				 product_table.getItems().setAll(filteredList);
+			 } catch (SQLException ex) {
+				 Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);
+			 }
+		 } else {
+			 product_table.getItems().clear();
+			 refresh();  
+
+		 }
+	 }
+
+	 private boolean matchesCriteria(ProductData product, String input) {
+		 String[] criteriaArray = input.split("\\s+");
+		 boolean matchesAllCriteria = true;
+
+		 for (String criterion : criteriaArray) {
+			 matchesAllCriteria = matchesAllCriteria &&
+					 (String.valueOf(product.getProductID()).contains(criterion)
+							 || product.getProductName().toLowerCase().contains(criterion)
+							 || product.getParentType().toLowerCase().contains(criterion)
+							 || product.getType().toLowerCase().contains(criterion)
+							 || product.getBrand().toLowerCase().contains(criterion)
+							 || product.getOtherAttributes().toLowerCase().contains(criterion));
+		 }
+
+		 return matchesAllCriteria;
+	 }
+
+	 private ObservableList<ProductData> retrieveProductsFromDatabase() throws SQLException {
+		 ObservableList<ProductData> productList = FXCollections.observableArrayList();
+
+		 try (Connection con = DatabaseManager.getConnection();
+				 Statement st = con.createStatement();
+				 ResultSet rs = st.executeQuery("SELECT * FROM Products join ProductCategory on Products.ProductID = ProductCategory.ProductID")) {
+
+			 while (rs.next()) {
+				 productList.add(new ProductData(
+						 rs.getInt("ProductID"),
+						 rs.getString("ProductName"),
+						 rs.getDouble("Price"),
+						 rs.getInt("Stocks"),
+						 rs.getString("Description"),
+						 (Blob) rs.getBlob("Image"),
+						 rs.getString("parentType"),
+						 rs.getString("type"),
+						 rs.getString("Brand"),
+						 rs.getString("otherAttributes")
+						 ));
+			 }
+		 }
+
+		 return productList;
+	 }
+
+	 @FXML
+	 private void Close(MouseEvent event) {
+		 Stage stage = (Stage) ((ImageView) event.getSource()).getScene().getWindow();
+		 stage.close();
+	 }
+
 }
 
